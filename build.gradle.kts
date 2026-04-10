f54cb9520f5cc60df08ca7a5e527afa2fba5e722
Somnath Kurmi <drn.today@outlook.com>
2026-04-10 13:06:53 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("com.android.library") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    // ✅ Updated from 1.9.0 to 1.9.23 to match Compose Compiler 1.5.11
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
