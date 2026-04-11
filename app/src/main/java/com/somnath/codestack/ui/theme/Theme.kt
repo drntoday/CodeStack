1b183998026244198ce01c889b8c03cca86db24e
Somnath Kurmi <drn.today@outlook.com>
2026-04-11 11:09:30 +0530

    Create Theme.kt

---
+++ app/src/main/java/com/somnath/codestack/ui/theme/Theme.kt
@@ -0,0 +1,52 @@
+package com.somnath.codestack.ui.theme
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color(0xFF00E5FF),
+    secondary = Violet,
+    tertiary = Color(0xFFFFAB40),
+    surface = Color(0xFF1E293B),
+    background = DeepSlate
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = ElectricBlue,
+    secondary = Violet,
+    tertiary = Pink
+    // Define light colors if necessary
+)
+
+@Composable
+fun CodeStackTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = AppTypography,
+        content = content
+    )
+}
