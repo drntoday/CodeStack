5694b671525b92ec3a5b8d2edd50fc0a7104030e
Somnath Kurmi <drn.today@outlook.com>
2026-04-11 11:08:06 +0530

    Create Color.kt

---
+++ app/src/main/java/com/somnath/codestack/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.somnath.codestack.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DeepSlate = Color(0xFF0f172a)
+val ElectricBlue = Color(0xFF3B82F6)
+val Violet = Color(0xFF8B5CF6)
+val SlateCard = Color(0xFF64748B)
+val Emerald = Color(0xFF10b981)
+val TerminalGreen = Color(0xFF00FF00)
