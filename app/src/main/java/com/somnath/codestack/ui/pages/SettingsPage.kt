5e6c94c075f12389f640c33bc8678e19d3af9325
Somnath Kurmi <drn.today@outlook.com>
2026-04-11 11:46:40 +0530

    Update SettingsPage.kt

--- app/src/main/java/com/somnath/codestack/ui/pages/SettingsPage.kt
+++ app/src/main/java/com/somnath/codestack/ui/pages/SettingsPage.kt
@@ -1,10 +1,12 @@
 package com.somnath.codestack.ui.pages
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -26,6 +28,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.somnath.codestack.Screen
 import com.somnath.codestack.ui.theme.DeepSlate
 import com.somnath.codestack.util.getApiKey
 import com.somnath.codestack.util.getGitHubToken
