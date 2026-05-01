c47fef277ba693f3e830233153353c29c3cad60e
Somnath Kurmi <drn.today@outlook.com>
2026-05-01 10:54:06 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Disabling source maps reduces memory usage significantly
-  productionBrowserSourceMaps: false,
-  experimental: {
-    turbo: false,
-  },
-  // Ensure the build doesn't hang on linting/type-checking if memory is tight
+  serverExternalPackages: ["jose"],
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 };
 
 export default nextConfig;
