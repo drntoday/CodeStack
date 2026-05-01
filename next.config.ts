fbb4cd2c48fea3fb60d9b60e5850bf1095bdd656
Somnath Kurmi <drn.today@outlook.com>
2026-05-01 11:06:14 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    externalPackages: ["jose"],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
